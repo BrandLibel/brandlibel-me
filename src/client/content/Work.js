370c3d88b2068fa126cb6994bae235dde3529a1e
Brandon Li <brandon.li@outlook.com>
2020-07-01 01:03:41 -0400

    WIP - showing an array of components

--- src/client/content/Work.js
+++ src/client/content/Work.js
@@ -2,8 +2,25 @@ import React from 'react';
 import Box from "./components/Box";
 
 function WorkList() {
+	let boxes = [];
+
+	boxes.push(
+		<Box color={global.COLORS.BLUE} wide key="1"/>
+	);
+	boxes.push(
+		<Box color={global.COLORS.ORANGE} wide key="2"/>
+	);
+	boxes.push(
+		<Box color={global.COLORS.BLUE} wide key="3"/>
+	);
+	boxes.push(
+		<Box color={global.COLORS.ORANGE} wide key="4"/>
+	);
+
 	return (
-		<div></div>
+		<div className="boxGrid">
+			{boxes}
+		</div>
 		// TODO: create, from a json file, a list of sortable WorkItemBox(?) components
 	);
 }
