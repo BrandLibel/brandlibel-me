58ea77e58834d91bb61d7140a8bc86b4e8c95148
Brandon Li <brandon.li@outlook.com>
2020-07-04 19:14:03 -0400

    Add the banner image for my website project

--- src/client/content/Work.js
+++ src/client/content/Work.js
@@ -4,6 +4,7 @@ import styles from "../styles/module/Work.module.css";
 
 import jsonProjects from "../data/json/projects.json5";
 
+import imgPathWebsite from "../data/img/banner_website.png";
 import imgPathNinjam from "../data/img/game_ninjam.png";
 import imgPathTerror from "../data/img/game_terror.png";
 import imgPathSpouse from "../data/img/game_spouse.png";
@@ -13,6 +14,7 @@ import imgPathInProgress from "../data/img/game_inProgress.png";
 
 function idToPath(projectId) {
 	switch (projectId) {
+		case "brandlibel-me": return imgPathWebsite;
 		case "ninjam-io": return imgPathNinjam;
 		case "project-terror": return imgPathTerror;
 		case "project-spouse": return imgPathSpouse;
