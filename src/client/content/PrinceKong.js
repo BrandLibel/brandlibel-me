8f4ba6da8c9515372daf626b5cc3440f4eefbb27
Brandon Li <brandon.li@outlook.com>
2020-07-08 17:14:58 -0400

    Tweak to Prince Kong game page

--- src/client/content/PrinceKong.js
+++ src/client/content/PrinceKong.js
@@ -8,7 +8,14 @@ export default function PrinceKong() {
                 <h1>Prince Kong</h1>
             </Box>
             <Box color={global.COLORS.CLEAR} wide>
-                <iframe src="/game/prince-kong/index.html"></iframe>
+                <iframe id="iFramePrinceKong" src="/game/prince-kong/index.html"></iframe>
+            </Box>
+            <Box color={global.COLORS.CLEAR} wide>
+                <h2>Instructions</h2>
+                <p>Move left: Left arrow key OR tap left half of game</p>
+                <p>Move right: Right arrow key OR tap right half of game</p>
+                <h2>About</h2>
+                <p>When I was 16, I participated in a 48-hour game jam with 2 other people. We created Prince Kong, a simple HTML5 game which you can play above. It won the game jam's grand prize.</p>
             </Box>
         </div>
     );
