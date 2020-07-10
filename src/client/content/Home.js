cf704ea13457c9f67da2309d777b2a136b0f1d98
Brandon Li <brandon.li@outlook.com>
2020-07-10 15:55:00 -0400

    Mobile responsiv

--- src/client/content/Home.js
+++ src/client/content/Home.js
@@ -8,23 +8,26 @@ import { BoxTintImage } from "./components/Image";
 export default class Home extends React.Component {
 	render() {
 		return (
-			<div className="boxGrid">
-				<Box wide>
-					<h1>Brandon Li, Full Stack Developer</h1>
-					<p>Hello there! I'm Brandon, and I create websites, mobile apps, and games.</p>
-					<BoxButtonNav to="/work" label="See all my work" />
-					<BoxButtonA label="Contact me" href={`mailto:${genEmAddr()}`} />
-				</Box>
-
-				<Box color={global.COLORS.ORANGE} left>
-					<h2>Ninjam.io</h2>
-					<p>A real-time multiplayer ninja action game.</p>
-					<p>Node.js, Websockets, Express.js, MySQL, Redis, Pug, and Pixi.js</p>
-					<p>Play as a guest, or create an account to have your stats tracked!</p>
-					<BoxButtonA label="Play Ninjam.io" color={global.COLORS.ORANGE} href="https://ninjam.io" target="_blank" />
-				</Box>
-				<div className="boxImageContainer boxRight">
-					<BoxTintImage href="https://ninjam.io" src={imgPathNinjam} />
+			<div>
+				<div className="boxGrid">
+					<Box wide>
+						<h1>Brandon Li, Full Stack Developer</h1>
+						<p>Hello there! I'm Brandon, and I create websites, mobile apps, and games.</p>
+						<BoxButtonNav to="/work" label="See all my work" />
+						<BoxButtonA label="Contact me" href={`mailto:${genEmAddr()}`} />
+					</Box>
+				</div>
+				<div className="boxGrid">
+					<Box color={global.COLORS.ORANGE} left>
+						<h2>Ninjam.io</h2>
+						<p>A real-time multiplayer ninja action game.</p>
+						<p>Node.js, Websockets, Express.js, MySQL, Redis, Pug, and Pixi.js</p>
+						<p>Play as a guest, or create an account to have your stats tracked!</p>
+						<BoxButtonA label="Play Ninjam.io" color={global.COLORS.ORANGE} href="https://ninjam.io" target="_blank" />
+					</Box>
+					<div className="boxImageContainer boxRight">
+						<BoxTintImage href="https://ninjam.io" src={imgPathNinjam} />
+					</div>
 				</div>
 			</div>
 		);
