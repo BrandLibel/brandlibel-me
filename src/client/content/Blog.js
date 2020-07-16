e95f90527c4fecb2f1657bd766e490573f4a30fc
Brandon Li <brandon.li@outlook.com>
2020-07-16 16:39:59 -0400

    Blog page can fetch and display blog posts

--- src/client/content/Blog.js
+++ src/client/content/Blog.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
 import Box from "./components/Box";
 
 export default class Blog extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            posts: []
+        }
+    }
+    componentDidMount() {
+        fetch("/api/blog/all")
+        .then(response => response.json())
+        .then(data => {
+            console.dir(data);
+            let postComponents = data.map((post) => {
+                return (
+                    <Box color={global.COLORS.CLEAR} wide>
+                        <h2>{post.title}</h2>
+                        <p>{post.markdown}</p>
+                    </Box>
+                )
+            });
+            this.setState({
+                posts: postComponents
+            });
+        });
+    }
     render() {
         return (
             <div>
@@ -9,6 +33,7 @@ export default class Blog extends React.Component {
                     <Box color={global.COLORS.CLEAR} wide>
                         <h1>Blog Libel</h1>
                     </Box>
+                    { this.state.posts }
                 </div>
             </div>
         );
